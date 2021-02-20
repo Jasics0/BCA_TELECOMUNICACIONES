e143530dc8721993af77fa8db20498aaab712ceb
Henry David Martinez <57374507+henrymartinez296@users.noreply.github.com>
2021-02-20 18:41:29 -0500

    Update BCA_TELECOMUNICACIONES.java

--- BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
+++ BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
@@ -1,19 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bca_telecomunicaciones;
 
-/**
- *
- * @author henry
- */
 public class BCA_TELECOMUNICACIONES {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("juasjuas");
         System.out.println("Henrro gai");
