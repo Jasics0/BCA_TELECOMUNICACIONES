7f746fce62541279cc6be4eb4e6aab4570933835
Henry David Martinez <57374507+henrymartinez296@users.noreply.github.com>
2021-02-20 18:34:58 -0500

    Update BCA_TELECOMUNICACIONES.java

--- BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
+++ BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
@@ -17,6 +17,7 @@ public class BCA_TELECOMUNICACIONES {
     public static void main(String[] args) {
         System.out.println("juasjuas");
         System.out.println("Henrro gai");
+        System.out.println("Ole esta piola");
     }
 
 }
