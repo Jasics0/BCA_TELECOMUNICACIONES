d7bf33e50a9bf5710b18c3ed0a1cfdace80bc709
Javier Andres Charry <61464947+Jasics0@users.noreply.github.com>
2021-02-20 18:33:30 -0500

    xd

--- BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
+++ BCA_TELECOMUNICACIONES/src/bca_telecomunicaciones/BCA_TELECOMUNICACIONES.java
@@ -15,6 +15,8 @@ public class BCA_TELECOMUNICACIONES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("juasjuas");    }
-    
+        System.out.println("juasjuas");
+        System.out.println("Henrro gai");
+    }
+
 }
