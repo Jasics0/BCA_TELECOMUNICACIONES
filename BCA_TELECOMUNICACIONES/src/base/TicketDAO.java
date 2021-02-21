2f8e36b89bcd7fd5ca95a535a800cbc717cf2c34
Javier Andres Charry <61464947+Jasics0@users.noreply.github.com>
2021-02-21 15:35:41 -0500

    xd

--- BCA_TELECOMUNICACIONES/src/base/TicketDAO.java
+++ BCA_TELECOMUNICACIONES/src/base/TicketDAO.java
@@ -6,22 +6,33 @@ import java.text.SimpleDateFormat;
 
 import entidades.Ticket;
 
-public class TicketDAO extends Database{
-    
-    public void insertarTicket(Ticket ticket){
-        SimpleDateFormat df= new SimpleDateFormat("yyyy-MM-dd");
-        insertar("INSERT INTO Tickets VALUES('"+ticket.getCodigo()+"','"+df.format(ticket.getFecha_inicial())+"',null,'"+ticket.getCedula_cliente()+"','"+ticket.getTipo()+"','"+ticket.getNotas_problema()+"','"+ticket.getNotas_solucion()+"',"+1+","+ticket.getCalidad_servicio()+");");
+public class TicketDAO extends Database {
+
+    public void insertarTicket(Ticket ticket) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        insertar("INSERT INTO Tickets VALUES('" + ticket.getCodigo() + "','" + df.format(ticket.getFecha_inicial())
+                + "',null,'" + ticket.getCedula_cliente() + "','" + ticket.getTipo() + "','"
+                + ticket.getNotas_problema() + "','" + ticket.getNotas_solucion() + "'," + 1 + ","
+                + ticket.getCalidad_servicio() + ");");
     }
 
-    
-    public ResultSet consultarTickets(String codigo) {
-        return consulta("SELECT * FROM Tickets WHERE(codigo_ticket='" + codigo + "');");
+    public Ticket consultarTickets(String codigo) throws SQLException {
+
+        ResultSet rs = consulta("SELECT * FROM Tickets WHERE(codigo_ticket='" + codigo + "');");
+        rs.next();
+        Ticket t = new Ticket(rs.getDate(2), rs.getDate(3), rs.getString(4), rs.getString(5), rs.getString(6),
+                rs.getString(7), rs.getBoolean(8), rs.getInt(9));
+        t.setCodigo(rs.getString(1));
+        return t;
     }
 
-
-    public void editarTicket(String codigo, Ticket ticket){
-        int estado=(ticket.getEstado()) ?1 :0;
-        insertar("UPDATE Tickets SET (fecha_inicial='"+ticket.getFecha_inicial()+"',fecha_final='"+ticket.getFecha_final()+"',cedula_cliente='"+ticket.getCedula_cliente()+"',tipo='"+ticket.getTipo()+"',notas_problema='"+ticket.getNotas_problema()+"', notas_solucion='"+ticket.getNotas_solucion()+"',estado="+estado+", calidad_servicio="+ticket.getCalidad_servicio()+") WHERE(codigo_ticket='"+codigo+"');");
+    public void editarTicket(String codigo, Ticket ticket) {
+        int estado = (ticket.getEstado()) ? 1 : 0;
+        insertar("UPDATE Tickets SET (fecha_inicial='" + ticket.getFecha_inicial() + "',fecha_final='"
+                + ticket.getFecha_final() + "',cedula_cliente='" + ticket.getCedula_cliente() + "',tipo='"
+                + ticket.getTipo() + "',notas_problema='" + ticket.getNotas_problema() + "', notas_solucion='"
+                + ticket.getNotas_solucion() + "',estado=" + estado + ", calidad_servicio="
+                + ticket.getCalidad_servicio() + ") WHERE(codigo_ticket='" + codigo + "');");
     }
 
     public ResultSet consultarTickets() {
@@ -32,12 +43,10 @@ public class TicketDAO extends Database{
         return consulta("SELECT codigo_ticket,cedula_cliente,fecha_inicial,tipo,estado FROM Tickets WHERE(estado=1);");
     }
 
-
     public int numeroTickets() throws SQLException {
-        ResultSet num=consulta("SELECT COUNT(codigo_ticket) FROM Tickets;");
+        ResultSet num = consulta("SELECT COUNT(codigo_ticket) FROM Tickets;");
         num.next();
         return num.getInt(1);
     }
-
 
 }
